3d9aeab3efc662316be66e8a96f59a3364719e58
ap514-cst <apomojumder688@gmail.com>
2026-06-30 23:41:13 +0600

    solve

--- src/components/User_Auth/API.jsx
+++ src/components/User_Auth/API.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL:"https://voacabulary-website-back-end-2.onrender.com/api/auth/google"
+    baseURL:"https://voacabulary-website-back-end-2.onrender.com/api/auth"
 });
 export const googleAuth=(code)=>api.get(`/google?code=${code}`);
